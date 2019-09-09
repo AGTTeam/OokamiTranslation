6945e87656e3fcec8b3a5bf5b302b10598cab9b8
Illidanz <illidanrulez@gmail.com>
2019-09-09 20:50:01 +0200

    Merged bin extraction in hacktools

--- extract_bin.py
+++ extract_bin.py
@@ -1,5 +1,3 @@
-import codecs
-import os
 import game
 from hacktools import common, nds
 
@@ -16,25 +14,10 @@ def run():
         binrange = game.binrange[1]
         # Decompress binary
         decompfile = infile.replace(".bin", "_dec.bin")
-        print("Decompressing BIN ...")
+        common.logMessage("Decompressing BIN ...")
         nds.decompressBinary(infile, decompfile)
         infile = decompfile
 
     common.logMessage("Extracting BIN to", outfile, "...")
-    with codecs.open(outfile, "w", "utf-8") as out:
-        foundstrings = []
-        insize = os.path.getsize(infile)
-        with common.Stream(infile, "rb") as f:
-            # Skip the beginning and end of the file to avoid false-positives
-            f.seek(binrange[0])
-            while f.tell() < binrange[1] and f.tell() < insize - 2:
-                pos = f.tell()
-                check = game.detectShiftJIS(f, "cp932")
-                if check != "":
-                    if check not in foundstrings:
-                        common.logDebug("Found string at", pos)
-                        foundstrings.append(check)
-                        out.write(check + "=\n")
-                    pos = f.tell() - 1
-                f.seek(pos + 1)
+    foundstrings = nds.extractBinaryStrings(infile, outfile, binrange, game.detectShiftJIS, "cp932")
     common.logMessage("Done! Extracted", len(foundstrings), "lines")
