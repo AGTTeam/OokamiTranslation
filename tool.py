4dac595ed6e9380c5b2df5530d363bb0ba01b040
Illidanz <illidanrulez@gmail.com>
2020-05-03 08:05:47 +0200

    Fix repack

--- tool.py
+++ tool.py
@@ -73,7 +73,9 @@ def repack(no_rom, bin, dat, img, wsb, sub, force):
         if os.path.isdir(replacefolder):
             common.mergeFolder(replacefolder, outfolder)
         if force != "":
-            common.copyFile(outfolder + "data/script/" + force + ".wsb", outfolder + "data/script/event/ev_act/act_010_opening.wsb")
+            if not force.endswith(".wsb"):
+                force += ".wsb"
+            common.copyFile(outfolder + "data/script/" + force, outfolder + "data/script/event/ev_act/act_010_opening.wsb")
         subtitle = "My Year with Holo" if firstgame else "The Wind that Spans the Sea"
         nds.editBannerTitle(bannerfile, "Spice & Wolf\n" + subtitle + "\nASCII MEDIA WORKS")
         romf = romfile if os.path.isfile(romfile) else romfile.replace("holo", "holo2")
